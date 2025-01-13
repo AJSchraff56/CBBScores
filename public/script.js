52c52e72f9db5bf781883891f07e6cb942edb610
AJSchraff56 <ajschraff1@gmail.com>
2025-01-13 15:00:18 -0600

    Update script.js

--- public/script.js
+++ public/script.js
@@ -708,11 +708,5 @@ if (game.status.includes('1st') || game.status.includes('2nd') || game.status.in
     
 
     // Fetch scores on page load
-    fetchScores();
- 
-    // Set up auto-refresh
- setInterval(() => {
-    console.log("Refreshing scores...");
-    fetchScores();
-}, REFRESH_INTERVAL);
+    fetchScores().then(startAutoRefresh);
 });
