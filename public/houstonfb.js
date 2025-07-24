272d05f615cec17de2099bf6ab447ab3d0b93478
AJSchraff56 <ajschraff1@gmail.com>
2025-07-24 15:33:50 -0500

    Update houstonfb.js

--- public/houstonfb.js
+++ public/houstonfb.js
@@ -524,7 +524,7 @@ console.log("Top 25 Games:", top25Data);
     
         uniqueConferences.sort((a, b) => a.name.localeCompare(b.name));
     
-        conferenceFilter.innerHTML = '<option value="all">All Conferences</option>'; // Ensure 'All Conferences' is always first
+        conferenceFilter.innerHTML = '<option value="4">Big 12</option>'; // Big 12 always first
         uniqueConferences.forEach(({ id, name }) => {
             const option = document.createElement('option');
             option.value = id;
