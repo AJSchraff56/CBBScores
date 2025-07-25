74336e1970ec3aa64acbfbd5fe0a992c98d0f7b0
AJSchraff56 <ajschraff1@gmail.com>
2025-07-25 10:21:26 -0500

    Update templates.js

--- public/templates.js
+++ public/templates.js
@@ -1,1 +1,9 @@
+const templates = [
+  {
+    title: "Example Template",
+    image: "https://i.ibb.co/MxsvYcf7/Screenshot-2025-07-25-at-10-14-00-AM.png",
+    stripeLink: "https://buy.stripe.com/28o7tAcefdqMgxOdQT"
+  }
+];
 
+export default templates;
